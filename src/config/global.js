a416dfe42c701d98fb30ad3966754e6c63eb12a3
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2025-05-20 20:03:00 -0500

    Update empaquetados

--- src/config/global.js
+++ src/config/global.js
@@ -1,7 +1,9 @@
 export default {
   global: {
-    componenteFormativo: 'Nombre del recurso educativo',
-    descripcionCurso: 'Descripción del RED',
+    componenteFormativo:
+      'Fundamentos del ciberespacio, la ciberseguridad y los delitos informáticos',
+    descripcionCurso:
+      'Ese componente formativo establece las bases para comprender el ciberespacio, la ciberseguridad y los delitos informáticos. Se explorarán los conceptos esenciales de la seguridad digital, los riesgos asociados a la navegación en línea y las amenazas cibernéticas más comunes. Además, se analizarán las normativas vigentes y las estrategias de prevención para fortalecer la protección de la información y los sistemas informáticos.',
     imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
@@ -12,6 +14,10 @@ export default {
       {
         clases: ['banner-principal-decorativo-2'],
         imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+      },
+      {
+        clases: ['banner-principal-decorativo-3'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-3.svg'),
       },
     ],
   },
@@ -31,13 +37,28 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Titulo_de_primer_nivel',
+        titulo: 'Ciberespacio fundamentos y alcance',
         desarrolloContenidos: true,
         subMenu: [
+          // {
+          //   numero: '1.1',
+          //   titulo: 'Concepto del ciberespacio',
+          //   hash: 't_1_1',
+          // },
           {
             numero: '1.1',
-            titulo: 'Titulo_de_segundo_nivel',
+            titulo: 'Historia y evolución del ciberespacio',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Características del ciberespacio',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Tipos de ciberespacio',
+            hash: 't_1_3',
           },
         ],
       },
@@ -45,14 +66,73 @@ export default {
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Titulo_de_primer_nivel',
+        titulo: 'Principios, normativas e importancia de la ciberseguridad',
         desarrolloContenidos: true,
+        subMenu: [
+          // {
+          //   numero: '2.1',
+          //   titulo: 'Concepto e importancia de la ciberseguridad',
+          //   hash: 't_2_1',
+          // },
+          {
+            numero: '2.1',
+            titulo: 'Historia y evolución de la ciberseguridad',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Características de la ciberseguridad',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Normas y regulaciones internacionales',
+            hash: 't_2_3',
+          },
+          {
+            numero: '2.4',
+            titulo: 'Retos actuales en ciberseguridad',
+            hash: 't_2_4',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Titulo_de_primer_nivel',
+        titulo: 'Delitos informáticos y su impacto',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: 'Actores del cibercrimen',
+            hash: 't_3_1',
+          },
+          {
+            numero: '3.2',
+            titulo: 'Tipos de delitos informáticos',
+            hash: 't_3_2',
+          },
+          {
+            numero: '3.3',
+            titulo: 'Delitos informáticos en Colombia y regulación vigente',
+            hash: 't_3_3',
+          },
+          {
+            numero: '3.4',
+            titulo: 'Contexto mundial y tendencias actuales',
+            hash: 't_3_4',
+          },
+          {
+            numero: '3.5',
+            titulo: 'Desafíos globales en la lucha contra el cibercrimen',
+            hash: 't_3_5',
+          },
+          {
+            numero: '3.6',
+            titulo: 'Mecanismos de reporte y prevención',
+            hash: 't_3_6',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -86,7 +166,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CFA_01_217202206_DU.pdf',
       },
       {
         icono: 'fas fa-download',
@@ -102,28 +182,125 @@ export default {
   },
   complementario: [
     {
-      tema: 'Tema_complementario',
-      referencia: 'Referencia_complementario',
-      tipo: 'Tipo_complementario',
-      link: 'Link_complementario',
+      tema: '3.1. Actores del cibercrimen',
+      referencia:
+        'Ecosistema de Recursos Educativos Digitales SENA. (2022). Tipos de hackers [Video]. YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=Puk3JZ5R_Ic',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: '3.6. Mecanismos de reporte y prevención',
+      referencia:
+        'Ecosistema de Recursos Educativos Digitales SENA. (2022). Seguridad de la información [Video]. YouTube.',
+      tipo: 'Video',
+      link:
+        'https://www.youtube.com/watch?v=OC8x73OfV6s&ab_channel=EcosistemadeRecursosEducativosDigitalesSENA',
+    },
+    {
+      tema: '3.6. Mecanismos de reporte y prevención',
+      referencia:
+        'Ecosistema de Recursos Educativos Digitales SENA. (2021). Herramientas y estrategias de protección: antivirus gratuitos [Video]. YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=jqL1RwFft-0',
     },
   ],
   glosario: [
     {
-      termino: 'Termino_glosario',
-      significado: 'Significado_glosario',
+      termino: 'Ciberespacio',
+      significado: 'entorno digital global de interconexión.',
+    },
+    {
+      termino: 'Ciberseguridad',
+      significado: 'protección de sistemas y datos contra amenazas digitales.',
+    },
+    {
+      termino: 'Contraseña fuerte',
+      significado:
+        'una contraseña que es difícil de adivinar o descifrar. Normalmente, incluye una combinación de letras mayúsculas y minúsculas, números y caracteres especiales, y tiene una longitud considerable.',
+    },
+    {
+      termino: '<em>Firewall</em>',
+      significado:
+        'herramienta de seguridad que monitorea y controla el tráfico de red, permitiendo o bloqueando el acceso a un sistema o red según reglas de seguridad predefinidas.',
+    },
+    {
+      termino: 'Fraude electrónico',
+      significado:
+        'uso de la tecnología y de Internet para realizar actividades fraudulentas, como el robo de identidad, el acceso no autorizado a cuentas bancarias o el uso indebido de tarjetas de crédito para obtener beneficios económicos.',
+    },
+    {
+      termino: '<em>Hacking</em> ético',
+      significado:
+        'práctica de evaluar la seguridad de sistemas informáticos de manera legal y con permiso, buscando vulnerabilidades para corregirlas antes de que los ciberdelincuentes puedan explotarlas.',
+    },
+    {
+      termino: '<em>Malware</em>',
+      significado:
+        'abreviatura de “<em>software</em> malicioso”, es cualquier <em>software</em> diseñado para dañar, interrumpir o acceder a sistemas informáticos sin el consentimiento del propietario. Incluye virus, troyanos, gusanos, etc.',
+    },
+    {
+      termino: '<em>Phishing</em>',
+      significado: 'técnica de engaño para obtener información confidencial.',
+    },
+    {
+      termino: '<em>Ransomware</em>',
+      significado:
+        'programa maligno que bloquea archivos hasta recibir un pago.',
     },
   ],
   referencias: [
     {
-      referencia: 'Nombre_referencias',
-      link: 'Link_referencias',
+      referencia:
+        'Dempsey, J. X., & Carlin, J. P. (2024). Cybersecurity law fundamentals.',
+      link: '',
+    },
+    {
+      referencia:
+        'Departamento Administrativo de la Función Pública. (2009). Ley 1273 de 2009 - Por medio de la cual se modifica el Código Penal, se crea un nuevo bien jurídico tutelado, denominado "de la protección de la información y de los datos".',
+      link:
+        'https://www.funcionpublica.gov.co/eva/gestornormativo/norma.php?i=34492',
+    },
+    {
+      referencia:
+        'Europol. (2024). Internet Organised Crime Threat Assessment (IOCTA) 2024.',
+      link:
+        'https://www.europol.europa.eu/cms/sites/default/files/documents/Internet%20Organised%20Crime%20Threat%20Assessment%20IOCTA%202024.pdf ',
+    },
+    {
+      referencia:
+        'Ferrer, E. A. (2023). Estudios de cibercrimen. Ediciones Olejnik.',
+      link: '',
+    },
+    {
+      referencia:
+        'Lux, L. M., & Calderón, G. O. (2020). The crime of cyber fraud: Definition and delimitation. Revista Chilena de Derecho y Tecnología, 9(1), 151-184.',
+      link: '',
+    },
+    {
+      referencia:
+        'Madariaga, A. S. (2024). Código penal y código de procedimiento penal: Comentado jurisprudencialmente. Grupo Editorial Ibañez.',
+      link: '',
+    },
+    {
+      referencia:
+        'Ministerio de Tecnologías de la Información y las Comunicaciones (MinTIC). (2016, junio 29). Los delincuentes cibernéticos no toman vacaciones: consejos para que se proteja en la red. Ministerio de Tecnologías de la Información y las Comunicaciones.',
+      link:
+        'https://www.mintic.gov.co/portal/inicio/Sala-de-prensa/Noticias/15607:Los-delincuentes-ciberneticos-no-toman-vacaciones-consejos-para-que-se-proteja-en-la-red',
+    },
+    {
+      referencia:
+        'National Institute of Standards and Technology (NIST). (2024). The NIST Cybersecurity Framework (CSF) 2.0.',
+      link: 'https://nvlpubs.nist.gov/nistpubs/CSWP/NIST.CSWP.29.pdf',
+    },
+    {
+      referencia:
+        'Paredes, A. R. Z., Quevedo, I. M. S., & Chalacán, L. J. M. (2020). Seguridad informática en las PyMES de la ciudad de Quevedo. Journal of Business and Entrepreneurial Studies: JBES, 4(2), 232-241.',
+      link: '',
+    },
+    {
+      referencia:
+        'Paya-Santos, C., & Luque-Juárez, J. M. (2021). El sistema de inteligencia criminal ante las nuevas amenazas y oportunidades del ciberespacio. Revista Científica General José María Córdova, 19(36), 1121-1136.',
+      link: 'https://doi.org/10.21830/19006586.855',
     },
   ],
   creditos: [
@@ -147,10 +324,9 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Juan Carlos Camargo Solano',
-          cargo: 'Experta temática',
-          centro:
-            'Centro Nacional de Hotelería, Turismo y Alimentos - Regional Distrito Capital',
+          nombre: 'Javier Eduardo Díaz Machuca',
+          cargo: 'Experto temático',
+          centro: 'Centro de Comercio y Servicios - Regional Tolima',
         },
         {
           nombre: 'Viviana Esperanza Herrera Quiñonez',
@@ -164,22 +340,17 @@ export default {
       autores: [
         {
           nombre: 'Oscar Ivan Uribe Ortiz',
-          cargo: 'Diseñador web',
+          cargo: 'Diseñador <em>web</em>',
           centro: 'Centro de Comercio y Servicios - Regional Tolima',
         },
         {
           nombre: 'Juan Daniel Polanco Muñoz',
-          cargo: 'Diseñador web',
+          cargo: 'Diseñador <em>web</em>',
           centro: 'Centro de Comercio y Servicios - Regional Tolima',
         },
         {
-          nombre: 'Diego Fernando Velasco Güiza',
-          cargo: 'Desarrollador Fullstack',
-          centro: 'Centro de Comercio y Servicios - Regional Tolima',
-        },
-        {
-          nombre: 'Gilberto Junior Rodríguez Rodríguez',
-          cargo: 'Animador y productor audiovisual',
+          nombre: 'Sebastian Trujillo Afanador',
+          cargo: 'Desarrollador <em>Fullstack</em>',
           centro: 'Centro de Comercio y Servicios - Regional Tolima',
         },
         {
@@ -193,17 +364,12 @@ export default {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
-          nombre: 'Norma Constanza Morales Cruz',
-          cargo: 'Evaluadora de contenidos inclusivos y accesibles',
+          nombre: 'Jorge Eduardo Rueda Peña',
+          cargo: 'Evaluador de contenidos inclusivos y accesibles',
           centro: 'Centro de Comercio y Servicios - Regional Tolima',
         },
         {
           nombre: 'Jorge Bustos Gómez',
-          cargo: 'Validador y vinculador de recursos educativos digitales',
-          centro: 'Centro de Comercio y Servicios - Regional Tolima',
-        },
-        {
-          nombre: 'Javier Mauricio Oviedo',
           cargo: 'Validador y vinculador de recursos educativos digitales',
           centro: 'Centro de Comercio y Servicios - Regional Tolima',
         },
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -2,35 +2,190 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    .titulo-principal.color-acento-contenido(data-aos="fade-right")
+      .titulo-principal__numero
+        span 1
+      h1 Ciberespacio fundamentos y alcance
     
-    //- El componente "Muestras" tiene las aplicaciones de todos los componentes de la base
-    //- se encuentra en src/components/Muestras.vue
-    //- Una vez el componente "Muestras" no se necesite 
-    //- Se debe borrar el "import Muestras from '../components/Muestras'" y en "components" en "<script"
-    //- esto evitará que se compile en la carpeta final de distribución
-    Muestras
+    .row.justify-content-center.align-items-center.mb-5(data-aos="flip-up")
+      .col-12
+        .row.justify-content-center.align-items-center
+          .col-lg-9.col-12.mb-md-0.order-lg-1.order-2.mb-lg-0.mb-3
+            p El ciberespacio es un entorno digital compuesto por redes interconectadas a través de Internet y otros sistemas de comunicación. Este espacio permite la interacción social, el comercio, la educación y la administración pública, transformando la forma en que las personas trabajan, aprenden y se relacionan. Sin embargo, su crecimiento también ha generado desafíos en seguridad digital y privacidad.
+            p Más allá de la conectividad entre dispositivos, el ciberespacio se sustenta en una infraestructura tecnológica compleja que incluye servidores, centros de datos, computación en la nube y redes descentralizadas. Este ecosistema ha impulsado el desarrollo de la economía digital, dando lugar a nuevas formas de negocio como el comercio electrónico, las criptomonedas y la inteligencia artificial aplicada.
+            .tarjeta.tarjeta--gris1.p-4
+              .row.justify-content-center.align-items-center
+                .col-auto.d-none.d-lg-block
+                  figure
+                    img(src='@/assets/curso/temas/tema1/img02.svg', alt='Imagen decorativa')
+                .col-10.mb-3
+                  p.mb-0 Las amenazas en el ciberespacio han evolucionado junto con la tecnología. Actualmente, los ataques cibernéticos incluyen <em>ransomware</em>, inteligencia artificial maliciosa, ingeniería social y espionaje digital. Estos riesgos afectan no solo a individuos y empresas, sino también a gobiernos, planteando desafíos en ciberseguridad y gobernanza digital.
 
-</template>
+          .col-lg-3.col-6.col-md-6.col-8.order-lg-2.order-1.mb-lg-0.mb-3.d-none.d-lg-block
+            figure
+              img(src='@/assets/curso/temas/tema1/img01.png', alt='Imagen decorativa')
+
+    .row.justify-content-center.align-items-center.mb-0(data-aos="flip-up")
+      .col-12
+        .row.justify-content-center.align-items-center
+          .col-lg-9.col-12.mb-md-0.order-lg-2.order-2.mb-lg-0.mb-3
+            p Debido a su naturaleza global, la regulación del ciberespacio es un reto en constante evolución. Organismos como la ONU, la Unión Europea y la OEA han establecido marcos legales y políticas de cooperación internacional para abordar delitos cibernéticos y proteger los derechos digitales. Sin embargo, el avance tecnológico supera muchas veces las regulaciones existentes, lo que exige un enfoque dinámico en la legislación.
+            .row
+              .col-auto.d-none.d-lg-block
+                figure
+                  img(src='@/assets/curso/temas/tema1/img04.png', alt='Imagen decorativa')
+              .col-10.mb-3
+                p.mb-0 Comprender el ciberespacio es esencial para navegar de manera segura en un mundo digitalizado y para enfrentar los desafíos que surgen en este entorno en constante transformación.
+
+          .col-lg-3.col-6.col-md-6.col-8.order-lg-1.order-1.mb-lg-0.mb-3
+            figure
+              img(src='@/assets/curso/temas/tema1/img03.png', alt='Imagen decorativa')
+    
+    //- separador
+    //- #t_1_1.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+    //-   h2 1.1 Concepto del ciberespacio
+
+    separador
+    #t_1_1.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 1.1 Historia y evolución del ciberespacio
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="flip-up")
+      .col-12
+        .row.justify-content-center.align-items-center
+          .col-lg-8.col-12.mb-md-0.order-lg-2.order-2.mb-lg-0.mb-3
+            p El ciberespacio es el resultado de décadas de avances en informática y telecomunicaciones. Su origen se remonta a la creación de las primeras computadoras electrónicas en la década de 1940, pero fue con el desarrollo de redes interconectadas cuando comenzó a tomar forma.
+            p En 1969, la Agencia de Proyectos de Investigación Avanzados de Defensa de EE. UU. (DARPA) creó ARPANET, la primera red de computadoras, sentando las bases de lo que hoy conocemos como Internet. En la década de 1980, con la popularización de las computadoras personales, el acceso a las redes se amplió y permitió la comunicación digital a mayor escala.
+            .row
+              .col-auto.d-none.d-lg-block
+                figure
+                  img(src='@/assets/curso/temas/tema1/img06.png', alt='Imagen decorativa')
+              .col-10.mb-3
+                p.mb-0 El gran punto de inflexión llegó en 1991 con la invención de la World Wide Web (WWW) por Tim Berners-Lee, que facilitó la navegación de información en línea y aceleró la expansión del ciberespacio. A partir de mediados de los años 90, el auge de los foros, chats y redes sociales transformó este entorno en un espacio de interacción global.
+
+          .col-lg-4.col-6.col-md-6.col-8.order-lg-1.order-1.mb-lg-0.mb-3
+            figure
+              img(src='@/assets/curso/temas/tema1/img05.png', alt='Imagen decorativa')
+
+    .bloque-texto-g.bloque-texto-g--inverso.color1.p-3.p-sm-4.p-md-5.mb-0(data-aos="fade-down")
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/temas/tema1/img07.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 Durante las décadas de 2000 y 2010, el crecimiento del ciberespacio fue impulsado por la proliferación de los teléfonos inteligentes, el comercio electrónico y plataformas como Facebook, Twitter y YouTube. Sin embargo, esta expansión también trajo desafíos como la seguridad en línea, la privacidad y la regulación de los derechos digitales.
+        br
+        br
+        p.mb-0 Hoy en día, el ciberespacio sigue evolucionando con tecnologías emergentes como la inteligencia artificial, el Internet de las Cosas (IoT) y la realidad virtual, consolidándose como un entorno indispensable para la sociedad, aunque también plantea retos significativos en materia de ciberseguridad y protección de datos.
+
+      
+    separador
+    #t_1_2.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 1.2 Características del ciberespacio
+
+    p.mb-5 El ciberespacio es un entorno digital dinámico que ha transformado la manera en que las personas interactúan, trabajan y acceden a la información. Su naturaleza única lo diferencia de otros espacios de comunicación y transacción, ya que no está limitado por fronteras físicas ni restricciones temporales. A continuación, se presentan sus principales características:
+
+    .tarjeta.tarjeta--BG02.px-md-5.px-4
+      .row.justify-content-center.align-items-center(data-aos="fade-down")
+        .col-lg-3.d-none.d-lg-block
+          figure(data-aos="zoom-in")
+            img(src="@/assets/curso/temas/tema1/img08.png", alt="alt").img400.m-auto
+        .col-lg-9
+          SlyderF.custom(columnas="col-lg-6 col-xl-4")
+            .tarjeta.tarjeta--slyder.p-4
+              .row.justify-content-center.mb-3
+                .col-6.col-lg-8
+                  img(src="@/assets/curso/temas/tema1/img09.svg", alt="alt").img100.m-auto
+              h4.titulo-1 Globalidad
+              p.mb-0 El ciberespacio trasciende fronteras geográficas y permite la interacción entre personas, organizaciones y sistemas en todo el mundo, facilitando la conectividad global.
+            .tarjeta.tarjeta--slyder.p-4
+              .row.justify-content-center.mb-3
+                .col-6.col-lg-8
+                  img(src="@/assets/curso/temas/tema1/img10.svg", alt="alt").img100.m-auto
+              h4.titulo-1 Interactividad
+              p.mb-0 No se limita a la recepción de información; los usuarios pueden generar, modificar y compartir contenido en tiempo real, fomentando la participación activa.
+            .tarjeta.tarjeta--slyder.p-4
+              .row.justify-content-center.mb-3
+                .col-6.col-lg-8
+                  img(src="@/assets/curso/temas/tema1/img11.svg", alt="alt").img100.m-auto
+              h4.titulo-1 Desmaterialización
+              p.mb-0 La información y las actividades en línea no dependen de un espacio físico, lo que permite la digitalización de documentos, servicios y transacciones.
+            .tarjeta.tarjeta--slyder.p-4
+              .row.justify-content-center.mb-3
+                .col-6.col-lg-8
+                  img(src="@/assets/curso/temas/tema1/img12.svg", alt="alt").img100.m-auto
+              h4.titulo-1 Accesibilidad
+              p.mb-0 Disponible las 24 horas del día desde diversos dispositivos conectados a la red, lo que facilita la inmediatez en el acceso a datos y servicios.
+            .tarjeta.tarjeta--slyder.p-4
+              .row.justify-content-center.mb-3
+                .col-6.col-lg-8
+                  img(src="@/assets/curso/temas/tema1/img13.svg", alt="alt").img100.m-auto
+              h4.titulo-1 Anonimato relativo
+              p.mb-0 Aunque existen mecanismos de identificación, los usuarios pueden interactuar sin revelar completamente su identidad, lo que puede ser tanto una ventaja como un riesgo.
+            .tarjeta.tarjeta--slyder.p-4
+              .row.justify-content-center.mb-3
+                .col-6.col-lg-8
+                  img(src="@/assets/curso/temas/tema1/img14.svg", alt="alt").img100.m-auto
+              h4.titulo-1 Virtualidad
+              p.mb-0 Aunque sus efectos pueden ser tangibles en la vida real, su existencia es completamente digital y no depende de estructuras físicas.
+            .tarjeta.tarjeta--slyder.p-4
+              .row.justify-content-center.mb-3
+                .col-6.col-lg-8
+                  img(src="@/assets/curso/temas/tema1/img15.svg", alt="alt").img100.m-auto
+              h4.titulo-1 Conectividad constante
+              p.mb-0 La interconexión entre sistemas, dispositivos y personas es permanente, lo que permite el flujo ininterrumpido de información y servicios.
+
+    separador
+    #t_1_3.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 1.3 Tipos de ciberespacio
+
+    p.mb-5 El ciberespacio no es un entorno uniforme, sino un ecosistema digital con múltiples áreas especializadas según su uso y accesibilidad. Se puede clasificar en función de sus propósitos, los actores involucrados y las regulaciones que lo rigen. A continuación, se presentan los principales tipos de ciberespacio:
+
+    .row.justify-content-center.align-items-center.mb-0(data-aos="flip-up")
+      .col-lg-10
+        .row.justify-content-center
+          .col-lg-8.col-12.mb-md-0
+            LineaTiempoD.color-acento-botones.especial
+              .row(numero="1" titulo="Ciberespacio público")
+                .col-12.mb-4.mb-md-0
+                  p Comprende las áreas de Internet de libre acceso para todos los usuarios, como sitios <em>web</em> informativos, redes sociales abiertas y foros públicos. Es un espacio de intercambio global donde la información circula sin restricciones significativas.
+              
+              .row(numero="2" titulo="Ciberespacio privado")
+                .col-12.mb-4.mb-md-0
+                  p Incluye redes digitales con acceso restringido, como sistemas internos de empresas, correos electrónicos corporativos y plataformas bancarias. La seguridad y la privacidad son esenciales en este entorno.
+
+              .row(numero="3" titulo="Ciberespacio comercial")
+                .col-12.mb-4.mb-md-0
+                  p Se centra en las actividades económicas y el comercio digital, abarcando tiendas en línea, plataformas de pago electrónico y servicios de suscripción. Su expansión ha transformado la forma en que se realizan transacciones a nivel global.
+
+              .row(numero="4" titulo="Ciberespacio gubernamental")
+                .col-12.mb-4.mb-md-0
+                  p Utilizado por entidades estatales para gestionar bases de datos oficiales, ofrecer servicios públicos y facilitar la comunicación con la ciudadanía. Su objetivo es optimizar la administración electrónica y garantizar la transparencia.
+
+              .row(numero="5" titulo="Ciberespacio militar o estratégico")
+                .col-12.mb-4.mb-md-0
+                  p Comprende la infraestructura digital utilizada para la defensa, la inteligencia y la seguridad nacional. Incluye sistemas de vigilancia, redes de comunicación militar y plataformas de ciberdefensa.
+
+              .row(numero="6" titulo="Ciberespacio educativo")
+                .col-12.mb-4.mb-md-0
+                  p Espacio destinado a la educación y la capacitación a través de plataformas de aprendizaje en línea, repositorios académicos y entornos virtuales. Su relevancia ha aumentado con la digitalización de la enseñanza.
+
+              .row(numero="7" titulo="Ciberespacio criminal")
+                .col-12.mb-4.mb-md-0
+                  p Incluye redes clandestinas y foros utilizados para actividades ilícitas, como el tráfico de datos, la ciberdelincuencia y los ataques informáticos. El "<em>dark web</em>" es un ejemplo representativo de este tipo de ciberespacio.
+
+          .col-lg-4.col-6.col-md-6.col-8.order-lg-2.order-1.mb-lg-0.mb-3.d-none.d-lg-block
+            figure
+              img(src='@/assets/curso/temas/tema1/img16.png', alt='Imagen decorativa')
+        
+
+
+  </template>
 
 <script>
-// eslint-disable-next-line prettier/prettier
-import Muestras from '../components/Muestras'; // borrar una vez el componente "Muestras" no se necesite
 export default {
   name: 'Tema1',
-  components: {
-    Muestras, // borrar una vez el componente "Muestras" no se necesite
-  },
   data: () => ({
-    // variables de vue
+    mostrarIndicador: true,
   }),
-  mounted() {
-    this.$nextTick(() => {
-      this.$aosRefresh()
-    })
-  },
-  updated() {
-    this.$aosRefresh()
-  },
 }
 </script>
 
